99cb63c17c07338d7bd0d2d5ea955e1bdf0aa2a6
minervamaga <minerva_magic_201@yahoo.com>
2020-03-10 21:37:56 -0500

    Initial upload

---
+++ LuckyShoes/ClassLibrary1/API.cs
@@ -0,0 +1,18 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LuckyShoes
+{
+    class API
+    {
+        public interface GenericModConfigMenuAPI
+        {
+            void RegisterModConfig(IManifest mod, Action revertToDefault, Action saveToFile);
+
+            void RegisterLabel(IManifest mod, string labelName, string labelDesc);
+            void RegisterSimpleOption(IManifest mod, string optionName, string optionDesc, Func<bool> optionGet, Action<bool> optionSet);
+        }
+    }
+}
---
+++ LuckyShoes/ClassLibrary1/ModConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+
+namespace LuckyShoes
+{
+    class ModConfig
+    {
+        public bool VerboseLogging { get; set; }
+    }
+}
